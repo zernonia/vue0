84d91909099c20eabd45fa2f47be64a2f5b6d4e1
zernonia <zernonia@gmail.com>
2023-12-23 23:41:27 +0800

    chore: add vueuse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@vueuse/nuxt'],
   shadcn: {
     /**
      * Prefix for all the imported component
