6321f2640253509527aa8852e0d6a5aeec9ddc42
zernonia <zernonia@gmail.com>
2023-12-23 23:41:56 +0800

    feat: read sse from clientside

--- app.vue
+++ app.vue
@@ -1,18 +1,51 @@
 <script setup lang="ts">
 import { GithubLogoIcon } from '@radix-icons/vue'
+
+const description = ref('Simple chat app')
+const content = ref('')
+async function handleSubmit() {
+  const completion = await $fetch<ReadableStream>('/api/create', {
+    method: 'POST',
+    body: {
+      description: description.value,
+    },
+    responseType: 'stream',
+  })
+
+  const reader = completion.getReader()
+  const decoder = new TextDecoder('utf-8')
+
+  const read = async (): Promise<void> => {
+    const { done, value } = await reader.read()
+    if (done) {
+      console.log('release locked')
+      return reader.releaseLock()
+    }
+
+    const chunk = decoder.decode(value, { stream: true })
+    content.value += chunk
+    return read()
+  }
+  await read()
+}
 </script>
 
 <template>
-  <div class="h-screen w-screen flex items-center justify-center">
-    <UiCard class="w-64">
+  <div class="h-screen w-screen flex flex-col items-center justify-center">
+    <UiCard class="w-96 mb-8">
       <UiCardHeader>
         <UiCardTitle>vue0</UiCardTitle>
       </UiCardHeader>
 
       <UiCardContent>
-        <p class="text-muted-foreground">
-          Work in progress
-        </p>
+        <div class="flex items-center gap-2">
+          <UiInput v-model="description" />
+          <UiButton :disabled="!description.length" @click="handleSubmit">
+            Send
+          </UiButton>
+        </div>
+
+        {{ content }}
       </UiCardContent>
 
       <UiCardFooter class="justify-end">
