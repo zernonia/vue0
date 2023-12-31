6f2dae7199de39dca0f27013b82ff869178e7cb6
zernonia <zernonia@gmail.com>
2023-12-31 13:07:42 +0800

    chore: use custom theme for shikiji

--- components/OutputCode.client.vue
+++ components/OutputCode.client.vue
@@ -6,16 +6,48 @@ const props = defineProps<{
 }>()
 
 const highlighter = await getHighlighter({
-  themes: ['rose-pine'],
   langs: ['vue'],
+  themes: [
+    {
+      name: 'my-theme',
+      settings: [
+        {
+          scope: 'string',
+          settings: {
+            foreground: '#00D8FF',
+          },
+        },
+        {
+          scope: 'entity.other.attribute-name',
+          settings: {
+            foreground: '#E62286',
+          },
+        },
+        {
+          scope: 'keyword',
+          settings: {
+            foreground: '#9f9f9f',
+          },
+        },
+        {
+          scope: 'comment',
+          settings: {
+            foreground: '#9f9f9f',
+          },
+        },
+      ],
+      bg: 'hsl(var(--primary))',
+      fg: 'hsl(var(--secondary))',
+    },
+  ],
 })
 
 const code = computed(() => highlighter.codeToHtml(props.sfcString, {
   lang: 'vue',
-  theme: 'rose-pine',
+  theme: 'my-theme',
 }))
 </script>
 
 <template>
-  <div class="text-sm px-6 pb-6 absolute w-full overflow-y-auto h-full z-10 bg-[#191724] text-primary-foreground" v-html="code" />
+  <div class="text-sm px-6 pb-6 absolute w-full overflow-y-auto h-full z-10 bg-primary text-primary-foreground" v-html="code" />
 </template>
