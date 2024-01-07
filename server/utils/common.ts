d8880de9117026a2a036c5c34aa2d0fcddaa30ea
zernonia <zernonia@gmail.com>
2024-01-07 18:33:08 +0800

    chore: add env exampel

--- server/utils/common.ts
+++ server/utils/common.ts
@@ -4,7 +4,7 @@ import { OpenAI } from 'openai'
 export { upperFirst as capitalize } from 'scule'
 
 export function useOpenAI(event: H3Event<EventHandlerRequest>) {
-  const apiKey = event.node.req.headers['x-openai-key']?.toString() || process.env.NUXT_OPENAI_API_KEY
+  const apiKey = process.env.NUXT_OPENAI_API_KEY || event.node.req.headers['x-openai-key']?.toString()
   return new OpenAI({
     apiKey,
   })
