506570effca45e68a571046822dff7baa58f5560
zernonia <zernonia@gmail.com>
2024-01-09 09:51:34 +0800

    chore: cleanup

--- components/DialogDelete.vue
+++ components/DialogDelete.vue
@@ -1,43 +1,23 @@
 <script setup lang="ts">
-import { AlertDialogOverlay, DialogClose } from 'radix-vue'
-import { X } from 'lucide-vue-next'
-
 defineProps<{
   title?: string
   description?: string
-}>()
-
-defineEmits<{
-  (event: 'update:open', value: boolean): void
 }>()
 </script>
 
 <template>
   <UiAlertDialog>
-    <UiAlertDialogTrigger v-if="$slots.default" as-child>
+    <UiAlertDialogTrigger as-child>
       <slot />
     </UiAlertDialogTrigger>
 
     <UiAlertDialogContent>
-      <UiAlertDialogHeader v-if="title || description">
-        <UiAlertDialogTitle v-if="title">
-          {{ title }}
-        </UiAlertDialogTitle>
-        <UiAlertDialogDescription v-if="description">
-          {{ description }}
-        </UiAlertDialogDescription>
+      <UiAlertDialogHeader>
+        <UiAlertDialogTitle>{{ title }}</UiAlertDialogTitle>
+        <UiAlertDialogDescription>{{ description }}</UiAlertDialogDescription>
       </UiAlertDialogHeader>
 
-      <slot name="body" />
-
-      <DialogClose
-        class="absolute top-2 right-2 appearance-none items-center justify-center rounded-full focus:outline-none cursor-pointer"
-        aria-label="Close"
-      >
-        <X @click="$emit('update:open', false)" />
-      </DialogClose>
-
-      <UiAlertDialogFooter v-if="$slots.footer" class="sm:justify-center">
+      <UiAlertDialogFooter>
         <slot name="footer" />
       </UiAlertDialogFooter>
     </UiAlertDialogContent>
--- components/ui/alert-dialog/AlertDialogContent.vue
+++ components/ui/alert-dialog/AlertDialogContent.vue
@@ -19,7 +19,7 @@ const emitsAsProps = useEmitAsProps(emits)
 <template>
   <AlertDialogPortal>
     <AlertDialogOverlay
-      class="fixed inset-0 z-50 bg-background/20 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0"
+      class="fixed inset-0 z-50 bg-background/80 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0"
     />
     <AlertDialogContent
       v-bind="{ ...props, ...emitsAsProps }"
